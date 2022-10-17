027aa58be6631c19cf4111298bdb76683a7cd512
Saleh-387 <abdshaleh124@webmail.umm.ac.id>
2022-10-17 17:29:07 +0700

    Initialize movie catalogue automated testing implemented

---
+++ codecept.conf.js
@@ -0,0 +1,32 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// HEADLESS=true npx codecept run
+setHeadlessWhen(process.env.HEADLESS);
+
+/** @type {CodeceptJS.MainConfig} */
+exports.config = {
+  tests: 'e2e/**/*.spec.js',
+  output: 'e2e/outputs',
+  helpers: {
+    Puppeteer: {
+      url: 'http://127.0.0.1:9075',
+      show: true,
+      windowSize: '1200x900',
+    },
+  },
+  include: {
+    I: './steps_file.js',
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'movie-catalogue',
+  plugins: {
+    retryFailedStep: {
+      enabled: true,
+    },
+    screenshotOnFail: {
+      enabled: true,
+    },
+  },
+};
